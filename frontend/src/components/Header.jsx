164fb36d6dc7a1c89a9d1f7d850e484346d13406
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-01-08 23:47:19 +0545

    Updatde the css in Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,15 +3,15 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20  '>
 
         {/* ------Leftside-----*/}
-        <div>
-            <p>
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+            <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
                 Book Appointment <br />With Trusted Doctors
             </p>
-            <div>
-                <img src={assets.group_profiles} alt="" />
+            <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+                <img className='w-28' src={assets.group_profiles} alt="" />
                 <p>Simply browse through our extensive list of trusted doctors, <br /> schedule your appointmnet hassle-free.</p>
 
 
@@ -22,8 +22,8 @@ const Header = () => {
         </div>
         {/*-------Rightside------*/}
 
-        <div>
-            <img src={assets.header_img} alt="" />
+        <div className='md:w-1/2 relative'>
+            <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
 
         </div>
 
