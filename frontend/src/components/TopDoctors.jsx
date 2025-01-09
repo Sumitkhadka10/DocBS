2b937124ed1d9da9c20831b05642a15a735e1f7c
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-01-09 10:41:17 +0545

    Updated the list of doctors in TopDoctor.jsx

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { doctors } from '../assets/assets';
 
 const TopDoctors = () => {
   return (
-    <div>
-        
+    <div className=''>
+      <h1>Top Doctors to Book</h1>
+      <p>Simply browse through our extensive list of trusted doctors.</p>
+      <div>
+        {doctors.slice(0, 10).map((item, index) => (
+          <div key={index}>
+            <img src={item.image} alt={item.name} />
+            <div>
+              <div>
+                <p>Status: </p>
+                <p>Available</p>
+              </div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button>More</button>
     </div>
-  )
-}
+  );
+};
 
-export default TopDoctors
+export default TopDoctors;
