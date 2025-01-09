362bd15880022d2e5ee9a1ea3bad6e91e2d5420c
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-01-09 11:59:12 +0545

    Created a Banner in Banner.jsx

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Banner = () => {
   return (
     <div>
-        
+        {/* ----Leftside--- */}
+        <div>
+            <div>
+                <p>Book Appointment</p>
+                <p>With lots of Trusted Doctors</p>
+            </div>
+            <button>Create Account</button>
+        </div>
+
+         {/* ----Rightside--- */}
+         <div>
+            <img src={assets.appointment_img} alt="" />
+
+        </div>
     </div>
   )
 }
