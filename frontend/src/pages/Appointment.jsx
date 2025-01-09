fb423b95ca964887a0cc1261c5efe03f90635eb8
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-01-09 20:07:22 +0545

    Updated Appointment.jsx and added its css

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const Appointment = () => {
-  return (
+
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+
+  const [docInfo,ssetDocInfo] = useState(null)
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id == docId)
+    ssetDocInfo(docInfo)
+    console.log(docInfo);
+    
+  }
+
+  useEffect(()=>{
+    fetchDocInfo()
+  },doctors,docId)
+
+
+  return docInfo && (
     <div>
-        
+        {/* --Doctor Details--- */}
+        <div className='flex flex-col sm:flex-row gap-4'>
+          <div>
+            <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+          </div>
+          <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+            {/* -----Doc info---- */}
+            <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+              {docInfo.name} 
+              <img src={assets.verified_icon} alt="" />
+              </p>
+              <div>
+                <p>{docInfo.degree} - {docInfo.speciality}</p>
+                <button>{docInfo.experience}</button>
+              </div>
+
+              {/*---About--- */}
+              <div>
+                <p>About <img src={assets.info_icon} alt="" /></p>
+                <p>{docInfo.about}</p>
+              </div>
+
+          </div>
+
+        </div>
     </div>
   )
 }
