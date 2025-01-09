c359d394f224f673979b7b0a9633f8d597782222
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-01-09 17:22:12 +0545

    Updated Footer section and added googlefonts in index.css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,18 +3,18 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <div>
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             {/* ...left.... */}
             <div>
-                <img src={assets.logo} alt="" />
-                <p>We aim to make healthcare accessible and convenient by connecting you with trusted doctors. Book appointments, manage schedules, and access health resources with ease. Our platform is designed to provide a seamless experience, ensuring data privacy and security.</p>
+                <img className='mb-5 w-48' src={assets.logo} alt="" />
+                <p className='w-full md:w-2/3 text-gray-600 leading-6'>We aim to make healthcare accessible and convenient by connecting you with trusted doctors. Book appointments, manage schedules, and access health resources with ease. Our platform is designed to provide a seamless experience, ensuring data privacy and security.</p>
 
             </div>
             {/* ...middle.... */}
             <div>
-                <p>Company</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>Company</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>Home</li>
                     <li>About us</li>
                     <li>Contact us</li>
@@ -24,8 +24,8 @@ const Footer = () => {
             </div>
             {/* ...right.... */}
             <div>
-                <p>Get in touch</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>Get in touch</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+9779813000000</li>
                     <li>Doctorbookingsystem@gmail.com</li>
                 </ul>
@@ -37,7 +37,7 @@ const Footer = () => {
             {/* ....Copyright Text.... */}
         </div>
         <hr />
-        <p>Copyright © 2025 DoctorBookingSystem - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright © 2025 DoctorBookingSystem - All Right Reserved.</p>
     </div>
   )
 }
