d2fa8bebe991bc42e49b98a8f2d4a6b0593492cb
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-01-25 19:20:29 +0545

    Added doctor appointment fee system

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -45,6 +45,9 @@ const Appointment = () => {
                 <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
                 <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
               </div>
+              <p>
+                Appointment fee: <span>{docInfo.fees}</span>
+              </p>
 
           </div>
 
