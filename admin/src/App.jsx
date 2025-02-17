92d74126c5b39e162d0747925f96983388b3b502
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-02-17 12:00:01 +0545

    Created different pages in Admin and routed that all

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorList from './pages/Admin/DoctorList';
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -12,6 +18,17 @@ const App = () => {
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
       <Navbar />
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorList />} />
+
+        </Routes>
+      </div>
 
     </div>
   ) : (
