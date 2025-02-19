927234d2825987639d69e358161cc099e4db44cb
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-02-19 15:30:23 +0545

    Created UserLogin and connected to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import validator from 'validator';
 import bcrypt from 'bcrypt';
+import userModel from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 
 
@@ -30,8 +32,57 @@ const registerUser = async (req,res) => {
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
 
+        const userData = {
+            name,
+            email,
+            password : hashedPassword
+        }
+
+        const newUser = new userModel(userData)
+        const user = await newUser.save()
+        //_id
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+
+        res.json({success:true,token})
+
+
 
     } catch (error) {
+        console.error(error);
+        res.json({success:false,message:error.message}) 
+        
         
     }
 }
+
+// API for user login
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await userModel.findOne({email})
+
+        if (!user) {
+            return res.json({success:false,message:"User not found"})
+            
+        } 
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if (isMatch) {
+
+            const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+            res.json({success:true,token})
+            
+        }  else {
+            res.json({success:false,message:"Invalid Credentials"})
+        }
+        
+    } catch (error) {
+        console.error(error);
+        res.json({success:false,message:error.message}) 
+        
+        
+    }
+}
+
+export {registerUser,loginUser}
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -10,11 +10,7 @@ const AppContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [doctors,setDoctors] = useState([])
 
-    const value = {
-        doctors,
-        currencySymbol
-
-    }
+    const [token,setToken] = useState('')
 
     const getDoctorsData = async () => {
         try {
@@ -32,6 +28,13 @@ const AppContextProvider = (props) => {
             
             
         }
+    }
+    const value = {
+        doctors,
+        currencySymbol,
+        token,setToken,
+        backendUrl
+
     }
 
     useEffect(()=>{
