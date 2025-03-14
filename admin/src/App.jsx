2eb82688c136a6ea07f035235206ae6a5e84e55e
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-03-14 12:31:43 +0545

    feat: Add doctor routes and context integration for dashboard and appointments

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,23 +11,38 @@ import AllAppointments from './pages/Admin/AllAppointments';
 import AddDoctor from './pages/Admin/AddDoctor';
 import DoctorList from './pages/Admin/DoctorList';
 import AdminUserList from './pages/Admin/AdminUserList';
+import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorAppointments from './pages/Doctor/DoctorAppointments';
+import DoctorProfile from './pages/Doctor/DoctorProfile';
+import DocNavbar from './components/DocNavbar';
+import DocSidebar from './components/DocSidebar';
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext)
 
-  return aToken ? (
+  return aToken || dToken ? (
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
-      <Navbar />
+      {aToken ? <Navbar /> : <DocNavbar />}
       <div className='flex items-start'>
-        <Sidebar />
+        {aToken ? <Sidebar /> : <DocSidebar />}
         <Routes>
+          {/* //Admin Routes */}
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
           <Route path='/all-appointments' element={<AllAppointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
           <Route path='/doctor-list' element={<DoctorList />} />
           <Route path='/admin-user-list' element={<AdminUserList />} />
+
+          {/* //Doctors Route */}
+
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
+
 
         </Routes>
       </div>
