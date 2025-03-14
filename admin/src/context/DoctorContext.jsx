071a19b5b654a52fb277a82932515a641f64b93e
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-03-14 18:39:11 +0545

    fix: Updated getAppointments function to fetch doctor appointments from the correct API endpoint

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -13,25 +13,27 @@ const DoctorContextProvider = (props) => {
     const getAppointments = async () => {
         try {
 
-            const { data } = axios.get(backendUrl + '/api/doctor/profile', { headers: { dToken } })
+            const { data } = await axios.get(backendUrl + '/api/doctor/appointments', { headers: { dToken } })
             if (data.success) {
                 setAppointments(data.appointments.reverse())
-
+                console.log(data.appointments.reverse());
             } else {
                 toast.error(data.message)
             }
-
         } catch (error) {
             console.log(error);
             toast.error(error.message)
-
         }
     }
 
+    
     const value = {
         dToken,
         setDToken,
-        backendUrl
+        backendUrl,
+        appointments,
+        setAppointments,
+        getAppointments
 
 
     }
