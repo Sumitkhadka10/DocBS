648a3f814d63b44d644e1daae81804f20fe434bd
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-03-14 12:55:47 +0545

    feat: Updated sidebar profile link text to 'My Profile'

--- admin/src/components/DocSidebar.jsx
+++ admin/src/components/DocSidebar.jsx
@@ -94,7 +94,7 @@ const DocSidebar = () => {
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                         </svg>
                                     </div>
-                                    <span className="leading-none">Profile</span>
+                                    <span className="leading-none">My Profile</span>
                                 </NavLink>
                             </div>
                         </div>
