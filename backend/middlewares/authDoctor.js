d4b3d062f24ddbca9db5e1e32170ae67d39f04b5
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-03-14 18:38:42 +0545

    fix: Corrected variable name for doctor token in authentication middleware

--- backend/middlewares/authDoctor.js
+++ backend/middlewares/authDoctor.js
@@ -4,11 +4,11 @@ import jwt from 'jsonwebtoken'
 
 const authDoctor = async (req,res,next) => {
     try {
-        const {dToken} = req.headers
-        if (!dToken) {
+        const {dtoken} = req.headers
+        if (!dtoken) {
             return res.json({success:false,message:'Not authorized Login Again'})
         }
-        const token_decode = jwt.verify(dToken,process.env.JWT_SECRET)
+        const token_decode = jwt.verify(dtoken,process.env.JWT_SECRET)
         req.body.docId = token_decode.id
 
         next()
