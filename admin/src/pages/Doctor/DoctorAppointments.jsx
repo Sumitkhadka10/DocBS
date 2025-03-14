6cc710562e92d3148878527733a2b089d238f84f
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-03-14 20:49:49 +0545

    feat: Integrate appointment completion and cancellation functionality in DoctorAppointments component

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -4,15 +4,15 @@ import { AppContext } from '../../context/AppContext'
 import { assets } from '../../assets/assets'
 
 const DoctorAppointments = () => {
-    const { dToken, appointments, getAppointments } = useContext(DoctorContext)
-    const {calculateAge, slotDateFormat, currency} = useContext(AppContext)
+  const { dToken, appointments, getAppointments, completeAppointment, cancelAppointment } = useContext(DoctorContext)
+  const { calculateAge, slotDateFormat, currency } = useContext(AppContext)
 
-    useEffect(() => {
-      if (dToken) {
-        getAppointments()
-        
-      }
-    },[dToken])
+  useEffect(() => {
+    if (dToken) {
+      getAppointments()
+
+    }
+  }, [dToken])
 
   return (
     <div className='w-full max-w-6xl m-5'>
@@ -27,19 +27,26 @@ const DoctorAppointments = () => {
           <p>Action</p>
         </div>
         {
-          appointments.map ((item,index)=>(
+          appointments.reverse().map((item, index) => (
             <div className='flex flex-wrap justify-between max:sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_2fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50' key={index}>
-              <p className='max-sm:hidden'>{index+1}</p>
+              <p className='max-sm:hidden'>{index + 1}</p>
               <div className='flex items-center gap-2'>
                 <img className='w-8 rounded-full' src={item.userData.image} alt="" /><p>{item.userData.name}</p>
               </div>
               <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
               <p>{slotDateFormat(item.slotDate)},{item.slotTime}</p>
               <p>{currency} {item.amount}</p>
-              <div className='flex'>
-                <img className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
-                <img className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
-              </div>
+              {
+                item.cancelled
+                  ? <p className='text-red-600 text-xs font-bold'>Cancelled</p>
+                  : item.isCompleted
+                    ? <p className='text-blue-500 text-xs font-bold'>Completed</p>
+                    :
+                    <div className='flex'>
+                      <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+                      <img onClick={() => completeAppointment(item._id)} className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
+                    </div>
+              }
 
             </div>
           ))
