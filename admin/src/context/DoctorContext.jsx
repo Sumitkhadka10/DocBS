e57be825155269840e499ff0674dce9c1ba1e087
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-03-14 17:41:26 +0545

    Implemented getAppointments function and added appointments route for doctors

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,11 +1,32 @@
 import { createContext, useState } from "react";
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 export const DoctorContext = createContext()
 
 const DoctorContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-    const [dToken, setDToken] = useState(localStorage.getItem('dToken')? localStorage.getItem('dToken') : '')
+    const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
+
+    const [appointments, setAppointments] = useState([])
+    const getAppointments = async () => {
+        try {
+
+            const { data } = axios.get(backendUrl + '/api/doctor/profile', { headers: { dToken } })
+            if (data.success) {
+                setAppointments(data.appointments.reverse())
+
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+
+        }
+    }
 
     const value = {
         dToken,
@@ -14,11 +35,11 @@ const DoctorContextProvider = (props) => {
 
 
     }
-     return (
-        <DoctorContext.Provider value ={value}>
+    return (
+        <DoctorContext.Provider value={value}>
             {props.children}
         </DoctorContext.Provider>
-     )
+    )
 
 }
 
