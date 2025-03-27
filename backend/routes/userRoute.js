5636493b1896dc23be5e4fb2031c983bef77efee
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-03-27 09:51:22 +0545

    feat: added nodemailer package and configure for email functionality

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,25 +1,40 @@
-import express from 'express';
-import { 
-  registerUser, loginUser, getProfile, updateProfile, bookAppointment, 
-  listAppointment, cancelAppointment, saveReportCard, getReportCards, 
-  updateReportCard, getNotifications, markNotificationRead 
-} from '../controllers/userController.js';
-import authUser from '../middlewares/authUser.js';
-import upload from '../middlewares/multer.js';
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getProfile,
+  updateProfile,
+  bookAppointment,
+  listAppointment,
+  cancelAppointment,
+  saveReportCard,
+  getReportCards,
+  updateReportCard,
+  getNotifications,
+  markNotificationRead,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/register', registerUser);
-userRouter.post('/login', loginUser);
-userRouter.get('/get-profile', authUser, getProfile);
-userRouter.post('/update-profile', upload.single('image'), authUser, updateProfile);
-userRouter.post('/book-appointment', authUser, bookAppointment);
-userRouter.get('/appointments', authUser, listAppointment);
-userRouter.post('/cancel-appointment', authUser, cancelAppointment);
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post("/update-profile", upload.single("image"), authUser, updateProfile);
+userRouter.post("/book-appointment", authUser, bookAppointment);
+userRouter.get("/appointments", authUser, listAppointment);
+userRouter.post("/cancel-appointment", authUser, cancelAppointment);
 userRouter.post("/save-report-card", authUser, saveReportCard);
 userRouter.get("/report-cards", authUser, getReportCards);
 userRouter.post("/update-report-card", authUser, updateReportCard);
-userRouter.get('/notifications', authUser, getNotifications);
-userRouter.post('/mark-notification-read', authUser, markNotificationRead);
+userRouter.get("/notifications", authUser, getNotifications);
+userRouter.post("/mark-notification-read", authUser, markNotificationRead);
+userRouter.get("/verify-email/:token", verifyEmail);
+userRouter.post("/forgot-password", forgotPassword);
+userRouter.post("/reset-password/:token", resetPassword);
 
 export default userRouter;
