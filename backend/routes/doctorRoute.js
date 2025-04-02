a601f04927ce49a7ecddbd768295834f8e2b3287
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-04-02 18:37:24 +0545

    feat: added change password endpoint for doctors

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,18 +1,31 @@
-import express from 'express';
-import { doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorProfile, updateDoctorProfile, doctorDashboard, getPatientReportCard, saveOrUpdatePatientReportCard } from '../controllers/doctorController.js';
-import authDoctor from '../middlewares/authDoctor.js';
+import express from "express";
+import {
+  doctorList,
+  loginDoctor,
+  appointmentsDoctor,
+  appointmentComplete,
+  appointmentCancel,
+  doctorProfile,
+  updateDoctorProfile,
+  doctorDashboard,
+  getPatientReportCard,
+  saveOrUpdatePatientReportCard,
+  changePassword,
+} from "../controllers/doctorController.js";
+import authDoctor from "../middlewares/authDoctor.js";
 
 const doctorRouter = express.Router();
 
-doctorRouter.get('/list', doctorList);
-doctorRouter.post('/login', loginDoctor);
-doctorRouter.get('/appointments', authDoctor, appointmentsDoctor);
-doctorRouter.post('/complete-appointment', authDoctor, appointmentComplete);
-doctorRouter.post('/cancel-appointment', authDoctor, appointmentCancel);
-doctorRouter.get('/profile', authDoctor, doctorProfile);
-doctorRouter.post('/update-profile', authDoctor, updateDoctorProfile);
-doctorRouter.get('/dashboard', authDoctor, doctorDashboard);
-doctorRouter.post('/patient-report-card', authDoctor, getPatientReportCard);
-doctorRouter.post('/save-or-update-report-card', authDoctor, saveOrUpdatePatientReportCard);
+doctorRouter.get("/list", doctorList);
+doctorRouter.post("/login", loginDoctor);
+doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
+doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
+doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
+doctorRouter.get("/profile", authDoctor, doctorProfile);
+doctorRouter.post("/update-profile", authDoctor, updateDoctorProfile);
+doctorRouter.get("/dashboard", authDoctor, doctorDashboard);
+doctorRouter.post("/patient-report-card", authDoctor, getPatientReportCard);
+doctorRouter.post("/save-or-update-report-card", authDoctor, saveOrUpdatePatientReportCard);
+doctorRouter.post("/change-password", authDoctor, changePassword);
 
 export default doctorRouter;
