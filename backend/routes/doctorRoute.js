d213af99eacc9708df07871d8bfb15f51b0ad144
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-04-03 14:59:02 +0545

    feat: added image upload functionality to update doctor profile route

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,19 +1,19 @@
 import express from "express";
 import {
-  doctorList,
-  loginDoctor,
-  appointmentsDoctor,
-  appointmentComplete,
-  appointmentCancel,
-  doctorProfile,
-  updateDoctorProfile,
-  doctorDashboard,
-  getPatientReportCard,
-  saveOrUpdatePatientReportCard,
-  changePassword,
+    doctorList,
+    loginDoctor,
+    appointmentsDoctor,
+    appointmentComplete,
+    appointmentCancel,
+    doctorProfile,
+    updateDoctorProfile,
+    doctorDashboard,
+    getPatientReportCard,
+    saveOrUpdatePatientReportCard,
+    changePassword,
 } from "../controllers/doctorController.js";
 import authDoctor from "../middlewares/authDoctor.js";
-
+import upload from "../middlewares/multer.js"; 
 const doctorRouter = express.Router();
 
 doctorRouter.get("/list", doctorList);
@@ -22,7 +22,7 @@ doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
 doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
 doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
 doctorRouter.get("/profile", authDoctor, doctorProfile);
-doctorRouter.post("/update-profile", authDoctor, updateDoctorProfile);
+doctorRouter.post("/update-profile", authDoctor, upload.single("image"), updateDoctorProfile);
 doctorRouter.get("/dashboard", authDoctor, doctorDashboard);
 doctorRouter.post("/patient-report-card", authDoctor, getPatientReportCard);
 doctorRouter.post("/save-or-update-report-card", authDoctor, saveOrUpdatePatientReportCard);
